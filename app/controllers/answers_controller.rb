82946565416734c40062c3d90b5d4a1fce799380
breaknenter <breaknenter@mail.ru>
2021-08-06 03:46:15 +0900

    Добавлен answers#create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,17 @@
 class AnswersController < ApplicationController
+  def create
+    @answer = question.answers.create(answer_params)
+  end
+
+  private
+
+  def question
+    @question ||= Question.find(params[:question_id])
+  end
+
+  helper_method :question
+
+  def answer_params
+    params.require(:answer).permit(:text)
+  end
 end
