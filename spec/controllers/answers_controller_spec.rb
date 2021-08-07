752a5751d007cd73a3a1b693209ddc9b8ca94627
breaknenter <breaknenter@mail.ru>
2021-08-07 17:45:57 +0900

    Исправлена спецификация AnswersController

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -14,13 +14,13 @@ RSpec.describe AnswersController, type: :controller do
 
     context 'with valid attributes' do
       it 'save new @answer in db' do
-        expect { valid_answer }.to change(question.answers, :count).by(1)
+        expect { valid_answer }.to change(Answer, :count).by(1)
       end
     end
 
     context 'with invalid attributes' do
       it 'not saved @answer in db' do
-        expect { invalid_answer }.to_not change(question.answers, :count)
+        expect { invalid_answer }.to_not change(Answer, :count)
       end
     end
   end
