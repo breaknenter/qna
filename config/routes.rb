2e08ecee0e7098695d55d807178526cc563a683c
breaknenter <breaknenter@mail.ru>
2021-08-11 01:54:37 +0900

    Установка маршрутов для devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'questions#index'
+
+  devise_for :users,
+             path_names: { 
+               sign_up:  :register,
+               sign_in:  :login,
+               sign_out: :logout
+             }
 
   resources :questions do
     resources :answers, only: %i[create]
