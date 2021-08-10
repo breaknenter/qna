5045b9d0421fa38a0e7fc1a08d46864ffd7a99d1
breaknenter <breaknenter@mail.ru>
2021-08-11 02:35:08 +0900

    Добавлен сценарий регистрации пользователя

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
+feature 'User can register', 'User registration for full functionality ' do
+  background { visit new_user_registration_path }
+
+  scenario 'Registration with valid data' do
+    fill_in 'Email',                 with: 'joeshmoe@mail.to'
+    fill_in 'Password',              with: 'qwerty12345'
+    fill_in 'Password confirmation', with: 'qwerty12345'
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'Registration with invalid data' do
+    fill_in 'Email',                 with: 'joeshmoe@mail.to'
+    fill_in 'Password',              with: 'qwerty12345'
+    click_button 'Sign up'
+
+    expect(page).to have_css('.alert')
+  end
+end
+
 feature 'User can login', %(
   To ask a question,
   the user must be logged in
