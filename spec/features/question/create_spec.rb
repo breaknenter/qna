f392df1acc33582e41d7097f54560752a170210c
breaknenter <breaknenter@mail.ru>
2021-08-11 22:44:40 +0900

    Добавлен сценарий создания вопроса для аутентифицированого пользователя с невалидными данными

--- spec/features/question/create_spec.rb
+++ spec/features/question/create_spec.rb
@@ -25,7 +25,17 @@ feature 'User can create question', %(
     expect(page).to have_content 'Test text'
   end
 
-  scenario 'Authenticated user creates a question with errors'
+  scenario 'Authenticated user creates a question with errors' do
+    visit new_user_session_path
+    fill_in 'Email',    with: 'someone@mail.to'
+    fill_in 'Password', with: 'qwerty12345'
+    click_button 'Log in'
+
+    visit new_question_path
+    click_button 'Ask'
+
+    expect(page).to have_content "Title can't be blank"
+  end
 
   scenario 'An unauthenticated user is trying to post a question' do
     visit questions_path
