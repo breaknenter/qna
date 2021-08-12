2e64b5509fea221b70459f10a805b1f47cd39a01
breaknenter <breaknenter@mail.ru>
2021-08-12 23:16:14 +0900

    Добавлен сценарий создания ответа на вопрос

---
+++ spec/features/answer/create_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'The user, being on the question page, can write an answer' do
+  given!(:question) { create(:question) }
+
+  describe 'authenticated user creates a answer' do
+    given(:user) { create(:user) }
+
+    background { sign_in(user) }
+    background { visit question_path question }
+
+    scenario 'create valid answer' do
+      fill_in 'Text', with: 'Answer text'
+      click_button 'Answer'
+
+      expect(page).to have_content 'Answer created'
+    end
+
+    scenario 'create invalid answer' do
+      click_button 'Answer'
+
+      expect(page).to have_content "Text can't be blank"
+    end
+  end
+
+  describe 'unauthenticated user creates a answer' do
+    scenario 'tries to answer a question' do
+      visit question_path question
+
+      expect(page).not_to have_button 'Answer'
+    end
+  end
+end
