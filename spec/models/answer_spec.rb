999d1390528c8059b1219b8f1f4c6bac8e4ff4fd
breaknenter <breaknenter@mail.ru>
2021-08-13 01:25:28 +0900

    Добавлены миграции (FK для моделей)

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   describe 'associations' do
+    it do
+      belong_to(:author)
+        .class_name('User')
+        .with_foreign_key('author_id')
+    end
     it { should belong_to(:question) }
   end
 
