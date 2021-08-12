8c806d06be40106e4282daa65900dc258f4244a2
breaknenter <breaknenter@mail.ru>
2021-08-13 00:53:18 +0900

    Реализован сценарий ответа на вопрос

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  describe 'POST #create' do
-    let(:question) { create(:question) }
+  let(:question) { create(:question) }
+
+  describe 'POST #create for authenticated user' do
+    let(:user) { create(:user) }
 
     let(:valid_answer) do
       post :create, params: { question_id: question, answer: attributes_for(:answer) }
@@ -11,6 +13,8 @@ RSpec.describe AnswersController, type: :controller do
     let(:invalid_answer) do
       post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
     end
+
+    before { login(user) }
 
     context 'with valid attributes' do
       it 'save new @answer in db' do
@@ -22,6 +26,16 @@ RSpec.describe AnswersController, type: :controller do
       it 'not saved @answer in db' do
         expect { invalid_answer }.to_not change(Answer, :count)
       end
+    end
+  end
+
+  describe 'POST #create for unauthenticated user' do
+    let(:answer) do
+      post :create, params: { question_id: question, answer: attributes_for(:answer) }
+    end
+
+    it 'save new @answer in db' do
+      expect { answer }.to_not change(Answer, :count)
     end
   end
 end
