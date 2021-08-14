ddd6775cc878293808bf2f7913e1cd19eacf574f
breaknenter <breaknenter@mail.ru>
2021-08-15 04:32:51 +0900

    Добавлена возможность удаления ответа

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -13,10 +13,23 @@ class AnswersController < ApplicationController
     end
   end
 
+  def destroy
+    if current_user.author?(answer)
+      answer.destroy
+      redirect_to question, notice: 'Your answer delete'
+    else
+      redirect_to question, alert: 'You cannot delete someone else answer'
+    end
+  end
+
   private
 
   def question
     @question ||= Question.find(params[:question_id])
+  end
+
+  def answer
+    @answer ||= Answer.find(params[:id])
   end
 
   helper_method :question
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
              }
 
   resources :questions do
-    resources :answers, only: %i[create]
+    resources :answers, only: %i[create destroy]
   end
 end
