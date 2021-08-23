b0413e4a72d1b4fdfabd404ffe3e0835ad3a79d7
breaknenter <breaknenter@mail.ru>
2021-08-24 07:23:18 +0900

    Добавлены сценарии для ответа: обновление с ошибкой и попытка редактирования чужого ответа

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -5,6 +5,10 @@ feature 'User can edit his answer' do
   given!(:question) { create(:question, author: user) }
   given!(:answer)   { create(:answer, question: question, author: user) }
 
+  given!(:user2)     { create(:user) }
+  given!(:question2) { create(:question, author: user2) }
+  given!(:answer2)   { create(:answer, question: question2, author: user2) }
+
   describe 'Authenticated user', js: true do
     before { sign_in(user) }
 
@@ -22,9 +26,23 @@ feature 'User can edit his answer' do
       end
     end
 
-    scenario 'edit his answer with errors'
+    scenario 'edit his answer with errors' do
+      visit question_path(question)
 
-    scenario 'try to edit other user answer'
+      within('.answers-list') do
+        click_link 'edit'
+        fill_in 'Text', with: ''
+        click_button 'save'
+      end
+
+      expect(page).to have_content  "Text can't be blank"
+    end
+
+    scenario 'try to edit other user answer' do
+      visit question_path(question2)
+
+      expect(page).to_not have_link 'edit'
+    end
   end
 
   scenario 'Unauthenticated user can not edit answer', js: true do
