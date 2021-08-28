fb55529ac149ce56e494fcf46d8d6e4a0d46726c
breaknenter <breaknenter@mail.ru>
2021-08-28 15:15:42 +0900

    В модели Question/Answer добавлены связи для best_answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,12 @@
 class Question < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :answers, dependent: :destroy
+  belongs_to :author,      class_name: 'User',   foreign_key: 'author_id'
+  belongs_to :best_answer, class_name: 'Answer', foreign_key: 'best_answer_id', dependent: :destroy, optional: true
+
+  has_many :answers, -> { where.not(id: best_answer_id) }, dependent: :destroy
 
   validates :title, :text, presence: true
+
+  def set_best_answer(answer)
+    update(best_answer_id: answer.id)
+  end
 end
