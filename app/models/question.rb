9d58d14203528b5997be6255f4d09d5c7fd5813f
breaknenter <breaknenter@mail.ru>
2021-08-30 17:41:59 +0900

    Добавлены тесты для моделей Question/Answer

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,11 +2,15 @@ class Question < ApplicationRecord
   belongs_to :author,      class_name: 'User',   foreign_key: 'author_id'
   belongs_to :best_answer, class_name: 'Answer', foreign_key: 'best_answer_id', dependent: :destroy, optional: true
 
-  has_many :answers, -> { where.not(id: best_answer_id) }, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   validates :title, :text, presence: true
 
   def set_best_answer(answer)
     update(best_answer_id: answer.id)
+  end
+
+  def answers_ex_best
+    best_answer_id ? answers.where.not(id: best_answer_id) : answers
   end
 end
