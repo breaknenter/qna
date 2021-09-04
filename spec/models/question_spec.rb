298a31c53549c495c270d1b47c9fac8e1667066d
breaknenter <breaknenter@mail.ru>
2021-09-04 19:57:27 +0900

    В тест модели Question добавлена связь для ActiveStorage

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -22,6 +22,10 @@ RSpec.describe Question, type: :model do
     it { have_many(:answers).dependent(:destroy) }
   end
 
+  it 'have many attached files' do
+    expect(described_class.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:text) }
