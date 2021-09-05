41fc4f8470e9a38921c5c337bbeda706d018b265
breaknenter <breaknenter@mail.ru>
2021-09-06 04:16:44 +0900

    Добавлен вид для FilesController#destroy

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,6 +1,8 @@
 class FilesController < ApplicationController
   before_action :authenticate_user!
 
+  helper_method :file
+
   def destroy
     # record -- связанная модель к которой прикреплён файл
     file.purge if current_user.author_of?(file.record)
