19e1bb47689f30cfcccebb1cf406815b0c7107b7
breaknenter <breaknenter@mail.ru>
2021-09-06 03:37:27 +0900

    Реализация FilesController#destroy

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,4 +1,14 @@
 class FilesController < ApplicationController
+  before_action :authenticate_user!
+
   def destroy
+    # record -- связанная модель к которой прикреплён файл
+    file.purge if current_user.author_of?(file.record)
+  end
+
+  private
+
+  def file
+    @file ||= ActiveStorage::Attachment.find(params[:id])
   end
 end
