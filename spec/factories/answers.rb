6a5eca50587276e8b6b0d8c1d2f1cdd87e4f485e
breaknenter <breaknenter@mail.ru>
2021-09-07 02:13:00 +0900

    Добавлен сценарий удаление файлк прикреплённого к ответу

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -10,5 +10,15 @@ FactoryBot.define do
     trait :invalid do
       text { nil }
     end
+
+    trait :with_file do
+      after :create do |answer|
+        answer.files.attach(
+          io:           File.open(Rails.root.join('spec', 'rails_helper.rb')),
+          filename:     'rails_helper.rb',
+          content_type: 'text/rb'
+        )
+      end
+    end
   end
 end
