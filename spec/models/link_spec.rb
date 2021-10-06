629d40cdf9967fb9253caa8973c9966e16d6b743
breaknenter <breaknenter@mail.ru>
2021-10-07 02:34:47 +0900

    В спецификацию модели Link добавлена валидация url

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -8,5 +8,10 @@ RSpec.describe Link, type: :model do
     describe 'validations' do
       it { should validate_presence_of :name }
       it { should validate_presence_of :url }
+
+      it { should     allow_value('https://onepagelove.com').for(:url) }
+      it { should_not allow_value('onepagelove.com')
+                        .for(:url)
+                        .with_message('Invalid URL format') }
     end
 end
