322eeb7af90ef8ec0d2562974303ec8fb50bf66b
breaknenter <breaknenter@mail.ru>
2021-10-08 01:25:43 +0900

    Добавлен метод LinksController#destroy

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,13 @@
 class LinksController < ApplicationController
+  before_action :authenticate_user!
+
+  def destroy
+    link.destroy if current_user.author_of?(link.question)
+  end
+
+  private
+
+  def link
+    @link ||= Link.find(params[:id])
+  end
 end
