982da15a7c0e72e0a3a8f76bf16b6e7e96c9ac80
breaknenter <breaknenter@mail.ru>
2021-10-08 01:13:45 +0900

    Добавлена спецификация для LinksController#destroy

---
+++ spec/controllers/links_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe LinksController, type: :controller do
+  let(:author)   { create(:user) }
+  let(:question) { create(:question, author: author) }
+  let(:user)     { create(:user) }
+
+  before do
+    question.links.create(name: 'Link', url: 'https://link.to')
+  end
+
+  describe 'DELETE #destroy' do
+    let(:delete_link) do
+        delete :destroy, params: { id: question.links.first }, format: :js
+    end
+
+    context 'author' do
+      before { login(author) }
+
+      it 'delete link' do
+        expect { delete_link }.to change(question.links, :count).by(-1)
+      end
+
+      it 'render :destroy' do
+        delete_link
+
+        expect(response).to render_template :destroy
+      end
+    end
+
+    context 'not author' do
+      before { login(user) }
+
+      it 'not delete link' do
+        expect { delete_link }.to_not change(question.links, :count)
+      end
+
+      it 'render :destroy' do
+        delete_link
+
+        expect(response).to render_template :destroy
+      end
+    end
+  end
+end
