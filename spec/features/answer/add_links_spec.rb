15b9ffbca94eec99e90d57d9f7fc281fd8318096
breaknenter <breaknenter@mail.ru>
2021-10-16 17:05:28 +0900

    Добавлен сценарий добавления ссылки к ответу

---
+++ spec/features/answer/add_links_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+feature 'The user can add links to the answer' do
+  given!(:user)        { create(:user) }
+  given!(:question)    { create(:question, author: user) }
+  given!(:link)        { 'onepagelove' }
+  given!(:url)         { 'https://onepagelove.com' }
+  given!(:invalid_url) { 'onepagelove.com' }
+
+  context 'authenticated user creating a answer' do
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'with valid link' do
+      within '.answer-create' do
+        fill_in 'Text', with: 'Answer text'
+
+        fill_in 'Link name', with: link
+        fill_in 'Url',       with: url
+
+        click_button 'Answer'
+      end
+
+      within '.answers-list' do
+        expect(page).to have_link link, href: url
+      end
+    end
+
+    scenario 'with invalid link' do
+    end
+  end
+end
