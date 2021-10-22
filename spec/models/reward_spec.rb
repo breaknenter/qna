a8837e46d7ee240afdb1fc43d06ca2f3e206d651
breaknenter <breaknenter@mail.ru>
2021-10-23 01:25:04 +0900

    Добавлена спецификация модели Reward

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Reward, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:question) }
+    it { should belong_to(:user).optional }
+
+    it 'have one attached file' do
+      expect(described_class.new.image).to be_an_instance_of(ActiveStorage::Attached::One)
+    end
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:image) }
+  end
 end
