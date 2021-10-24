65d1840e929f869508446aa662a640e68d9abec8
breaknenter <breaknenter@mail.ru>
2021-10-24 23:27:57 +0900

    Добавлена фабрика для Reward

--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :reward do
-    name { "MyString" }
+    name { 'Reward name' }
+    question
+    user
+
+    after(:build) do |reward|
+      reward.image.attach(
+        io:           File.open("#{Rails.root}/spec/files/reward.jpg"),
+        filename:     'reward.jpg',
+        content_type: 'image/jpg'
+      )
+    end
   end
 end
