47f16c9b2e12c7b0ba4dcc3cd645141f04cf6437
breaknenter <breaknenter@mail.ru>
2021-10-25 00:29:44 +0900

    Правки в spec/features/rewards/index_spec.rb

--- spec/features/rewards/index_spec.rb
+++ spec/features/rewards/index_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 feature 'User can see their rewards' do
-  given(:user)   { create(:user) }
-  given(:reward) { create(:reward, user: user) }
+  given!(:user)   { create(:user) }
+  given!(:reward) { create(:reward, user: user) }
 
   scenario 'Authenticated user views rewards' do
     sign_in(user)
     visit rewards_path
 
     expect(page).to have_content 'Rewards:'
-    expect(page).to have_link reward.question.title, href: "/questions/#{reward.question.id}"
+    expect(page).to have_link reward.question.title, href: question_path(reward.question)
     expect(page).to have_content reward.name
     expect(page).to have_css("img[src*='reward.jpg']")
   end
