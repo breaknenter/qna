dc7ccc8f74e0440ece148f1995aac49bcf44c540
breaknenter <breaknenter@mail.ru>
2021-10-26 23:12:18 +0900

    features/question/add_links_spec.rb - добавлен сценарий добавления ссылки на gist

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -5,6 +5,7 @@ feature 'The user can add links to the question' do
   given!(:link)         { 'onepagelove' }
   given!(:url)          { 'https://onepagelove.com' }
   given!(:invalid_url)  { 'onepagelove.com' }
+  given!(:gist_url)     { 'https://gist.github.com/breaknenter/297fdd1adf66023e67b752ddf96b37d4' }
 
   context 'authenticated user creating a question' do
     background do
@@ -22,6 +23,19 @@ feature 'The user can add links to the question' do
       click_button 'Ask'
 
       expect(page).to have_link link, href: url
+    end
+
+    scenario 'with valid link to gist', js: true do
+      fill_in 'Title', with: 'Test title'
+      fill_in 'Text',  with: 'Test text'
+
+      fill_in 'Link name', with: 'Gist'
+      fill_in 'Url',       with: gist_url
+
+      click_button 'Ask'
+
+      expect(page).to have_link    'test_gist.txt', href: gist_url
+      expect(page).to have_content 'What thou lovest well remains, the rest is dross'
     end
 
     scenario 'with invalid link' do
