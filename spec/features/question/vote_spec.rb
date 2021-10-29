de725d036305aae579564527bc425edf52272a6a
breaknenter <breaknenter@mail.ru>
2021-10-30 03:25:27 +0900

    Добавлен сценарий отображения счётчика голосов и ссылок для аутентифицированого пользователя

---
+++ spec/features/question/vote_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User can vote for a question' do
+  given!(:user) { create :user }
+  given!(:author) { create :user }
+  given!(:question) { create :question, author: author }
+
+  context 'authenticated user', js: true do
+    background do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario 'sees counter and links' do
+      within('.votes') do
+        expect(page).to have_content '0'
+        expect(page).to have_link '+'
+        expect(page).to have_link '-'
+      end
+    end
+  end
+end
