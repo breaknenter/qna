c6b4a8b3bbded15212721a81ca87ebe5f8e4890f
breaknenter <breaknenter@mail.ru>
2021-10-30 06:00:43 +0900

    Миграция для Vote

--- db/migrate/20211029183843_create_votes.rb
+++ db/migrate/20211029183843_create_votes.rb
@@ -1,6 +1,13 @@
 class CreateVotes < ActiveRecord::Migration[6.1]
   def change
     create_table :votes do |t|
+      t.integer :value, null: false
+
+      t.references :user, foreign_key: true, index: { unique: true }
+
+      t.references :votable, polymorphic: true
+
+      t.index %i[user_id votable_type votable_id], unique: true
 
       t.timestamps
     end
