736ee17cea77d35c1281773721db11470a22a558
breaknenter <breaknenter@mail.ru>
2022-03-07 17:22:21 +0900

    В тесты  моделей Question и Answer добавлена проверка ассоциации has_many :votes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe Answer, type: :model do
     end
 
     it { should have_many(:links).dependent(:destroy) }
+    it { should have_many(:votes).dependent(:destroy) }
 
     it { should accept_nested_attributes_for :links }
   end
