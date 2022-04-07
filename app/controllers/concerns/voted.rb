0be1992c40fea25640efddfb2618d75902504c98
breaknenter <breaknenter@mail.ru>
2022-04-08 01:15:49 +0900

    В консёрн Voted добавлен приватный метод render_vote

--- app/controllers/concerns/voted.rb
+++ app/controllers/concerns/voted.rb
@@ -8,13 +8,25 @@ module Voted
 
   def vote_up
     vote = @voteable.vote!(user: current_user, value: 1)
+
+    render_vote(vote)
   end
 
   def vote_down
     vote = @voteable.vote!(user: current_user, value: -1)
+
+    render_vote(vote)
   end
 
   private
+
+  def render_vote(vote)
+    if vote.errors.any?
+      render json: { errors: vote.errors.full_messages }, status: :unprocessable_entity
+    else
+      render json: { rating: vote.voteable.rating }
+    end
+  end
 
   def model_klass
     controller_name.classify.constantize
