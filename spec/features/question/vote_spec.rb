0b56d8bd1f5240abbc7f1d4f46dbf3ac7ced6dc5
breaknenter <breaknenter@mail.ru>
2022-04-13 03:23:09 +0900

    Добавлены дополнительные тесты для vote_spec

--- spec/features/question/vote_spec.rb
+++ spec/features/question/vote_spec.rb
@@ -22,5 +22,41 @@ feature 'User can vote for a question' do
         expect(page).to have_content '1'
       end
     end
+
+    scenario 'set double like' do
+      within('.votes') do
+        click_link '+'
+        click_link '+'
+
+        sleep 0.1
+
+        error = page.driver.browser.switch_to.alert.text
+        page.accept_alert
+
+        expect(error).to eq('User you have already voted')
+        expect(page).to have_content '1'
+      end
+    end
+  end
+
+  context 'authenticated author', js: true do
+    background do
+      sign_in(author)
+      visit question_path(question)
+    end
+
+    scenario 'set self-like' do
+      within('.votes') do
+        click_link '+'
+
+        sleep 0.1
+
+        error = page.driver.browser.switch_to.alert.text
+        page.accept_alert
+
+        expect(error).to eq('User self-like it for xxxx')
+        expect(page).to have_content '0'
+      end
+    end
   end
 end
