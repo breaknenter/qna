dfb367d97f2182f0b1664fcc330680d6e144e86f
breaknenter <breaknenter@mail.ru>
2022-08-16 10:01:32 +0900

    Добавлен сценарий появления ответа на странице гостя

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 feature 'The user, being on the question page, can write an answer' do
-  given(:user) { create(:user) }
+  given(:user)      { create(:user)  }
+  given(:guest)     { create(:guest) }
   given!(:question) { create(:question, author: user) }
 
   describe 'authenticated user creates a answer', js: true do
@@ -39,6 +40,33 @@ feature 'The user, being on the question page, can write an answer' do
       visit question_path question
 
       expect(page).not_to have_button 'Answer'
+    end
+  end
+
+  scenario 'Answer appears on another user page', js: true do
+    Capybara.using_session('user') do
+      sign_in(user)
+      visit question_path question
+
+      within '.answer-create' do
+        fill_in 'Text', with: 'Answer text'
+      end
+    end
+
+    Capybara.using_session('guest') do
+      visit question_path question
+    end
+
+    Capybara.using_session('user') do
+      within '.answer-create' do
+        click_button 'Answer'
+      end
+
+      expect(page).to have_content 'Answer text'
+    end
+
+    Capybara.using_session('guest') do
+      expect(page).to have_content 'Answer text'
     end
   end
 end
