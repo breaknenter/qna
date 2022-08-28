fff3a4cd5071f8c237a19ddf61c2a74e31e24048
breaknenter <breaknenter@mail.ru>
2022-08-28 17:38:53 +0900

    Добавлен уникальный составной индекс для provider и uid в таблице authorizations

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2022_08_26_131219) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["provider", "uid"], name: "index_authorizations_on_provider_and_uid", unique: true
     t.index ["user_id"], name: "index_authorizations_on_user_id"
   end
 
