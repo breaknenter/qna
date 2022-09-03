0dca012f2e9143c0a67e35f7a3df12c022bc3745
breaknenter <breaknenter@mail.ru>
2022-09-04 02:13:03 +0900

    Изменён сценарий регистрации нового пользователя

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,13 +3,16 @@ require 'rails_helper'
 feature 'User can register', 'User registration for full functionality ' do
   background { visit new_user_registration_path }
 
-  scenario 'Registration with valid data' do
+  scenario 'Registration with valid data and confirmation email' do
     fill_in 'Email',                 with: 'joeshmoe@mail.to'
     fill_in 'Password',              with: 'qwerty12345'
     fill_in 'Password confirmation', with: 'qwerty12345'
     click_button 'Sign up'
 
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    open_email('joeshmoe@mail.to')
+    current_email.click_link 'Confirm my account'
+
+    expect(page).to have_content 'Your email address has been successfully confirmed.'
   end
 
   scenario 'Registration with invalid data' do
