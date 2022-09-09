6bec3578c24c6da4f23cb62855bed87f2768a2bd
breaknenter <breaknenter@mail.ru>
2022-09-09 22:39:52 +0900

    Добавлены тесты для OauthCallbacksController#vkontakte

--- spec/controllers/oauth_callbacks_controller_spec.rb
+++ spec/controllers/oauth_callbacks_controller_spec.rb
@@ -48,5 +48,44 @@ RSpec.describe OauthCallbacksController, type: :controller do
   end
 
   describe 'Vkontakte' do
+    let(:oauth_data) do
+      OmniAuth::AuthHash.new(
+        provider: 'vkontakte',
+        uid:      '1234',
+        info: {
+          email: 'test@email.io'
+        }
+      )
+    end
+
+    let(:oauth) { OmniAuth.config.mock_auth[:provider] = oauth_data }
+
+    before { @request.env['omniauth.auth'] = oauth_data }
+
+    context 'if user exists' do
+      let(:user) { FindForOauthService.new(oauth).call }
+
+      before do
+        get :vkontakte
+      end
+
+      it 'login user' do
+        expect(subject.current_user).to eq user
+      end
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'if user does not exists' do
+      before do
+        get :vkontakte
+      end
+
+      it 'redirects to root path' do
+        expect(response).to redirect_to(root_path)
+      end
+    end
   end
 end
