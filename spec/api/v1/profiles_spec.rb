2b8c1e3a920dffcee2179362ade01f8afd7cf56f
breaknenter <breaknenter@mail.ru>
2022-10-08 20:45:01 +0900

    Добавлен метод ProfilesController#index, тесты, shared_examples

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -23,21 +23,53 @@ describe 'Profiles API', type: :request do
             headers: headers
       end
 
-      it 'returns 200 status' do
-        expect(response).to be_successful
+      it_behaves_like 'Status be_successful'
+
+      it_behaves_like 'Returns all public fields' do
+        let(:resource) { json['user'] }
+        let(:user) { me }
       end
 
-      it 'returns all public fields' do
-        %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
-        end
+      it_behaves_like 'Does not return private fields'
+    end
+  end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:users) { create_list(:user, 4) }
+      let(:me) { users.first }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id).token }
+
+      before do
+        get api_path,
+            params: { access_token: access_token },
+            headers: headers
       end
 
-      it 'does not return private fields' do
-        %w[password encrypted_password].each do |attr|
-          expect(json).to_not have_key(attr)
-        end
+      it_behaves_like 'Status be_successful'
+
+      it 'list of users does not include me' do
+        ids = json['users'].pluck('id')
+
+        expect(ids).to_not include(me.id)
       end
+
+      it 'returns list of users ex me' do
+        expect(json['users'].size).to eq 3
+      end
+
+      it_behaves_like 'Returns all public fields' do
+        let(:resource) { json['users'].last }
+        let(:user) { users.last }
+      end
+
+      it_behaves_like 'Does not return private fields'
     end
   end
 end
---
+++ spec/support/shared/api/api_attributes.rb
@@ -0,0 +1,15 @@
+shared_examples_for 'Returns all public fields' do
+  it 'returns all public fields' do
+    %w[id email admin].each do |attr|
+      expect(resource[attr]).to eq user.send(attr).as_json
+    end
+  end
+end
+
+shared_examples_for 'Does not return private fields' do
+  it 'does not return private fields' do
+    private_fields = %w[password encrypted_password]
+
+    expect(json).to_not have_key(private_fields)
+  end
+end
