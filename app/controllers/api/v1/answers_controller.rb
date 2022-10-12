2c01dd4df9a9a7b4ac5759644a8d2b61807fb5a6
breaknenter <breaknenter@mail.ru>
2022-10-12 14:11:53 +0900

    API: добавлен метод AnswersController#create, тесты

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,13 +1,33 @@
 class Api::V1::AnswersController < Api::V1::BaseController
-  before_action :answer, only: %i[create update show destroy]
+  before_action :set_answer, only: %i[create update show destroy]
+  before_action :set_question, only: :create
+
+  def create
+    @answer = current_resource_owner.answers.build(answer_params)
+    @answer.question = @question
+    
+    if @answer.save
+      render json: @answer, status: :created
+    else
+      render json: { errors: @answer.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 
   def show
-    render json: answer
+    render json: @answer
   end
 
   private
 
-  def answer
-    Answer.find(params[:id])
+  def set_answer
+    @answer ||= params[:id] ? Answer.find(params[:id]) : Answer.new
+  end
+
+  def set_question
+    @question ||= params[:question_id] ? Question.find(params[:question_id]) : @answer.question
+  end
+
+  def answer_params
+    params.require(:answer).permit(:text)
   end
 end
