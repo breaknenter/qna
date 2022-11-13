6bbbfc054322f6207902e6e405006be5e0e867c7
breaknenter <breaknenter@mail.ru>
2022-11-13 14:29:49 +0900

    К тестам модели Question добавлена проверка на создание подписки для автора

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -41,6 +41,10 @@ RSpec.describe Question, type: :model do
     expect(described_class.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
 
+  it 'create subscription for author' do
+    expect { question }.to change(user.subscriptions, :count).from(0).to(1)
+  end
+
   describe 'validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:text) }
