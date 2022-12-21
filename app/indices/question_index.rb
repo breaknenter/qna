73a5a8849be604f7e1cfab3d446e8a1b08dd70ec
breaknenter <breaknenter@mail.ru>
2022-12-21 14:51:07 +0900

    Правка значений ключей индексов

--- app/indices/question_index.rb
+++ app/indices/question_index.rb
@@ -1,7 +1,7 @@
 ThinkingSphinx::Index.define :question, with: :active_record do
   indexes title, sortable: true
   indexes text
-  indexes author.email, as: author, sortable: true
+  indexes author.email, as: :author, sortable: true
 
   has author_id, created_at, updated_at
 end
