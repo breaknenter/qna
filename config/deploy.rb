50b581757e4247aeb6a642070f067fc8bfee2adb
breaknenter <breaknenter@mail.ru>
2023-01-17 18:55:39 +0900

    config/deploy.rb: set :pty, false

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url,    'git@github.com:breaknenter/qna.git'
 set :branch,      'main'
 set :deploy_to,   '/home/deployer/qna'
 set :deploy_user, 'deployer'
+set :pty,         false
 
 set :linked_files, %w[config/database.yml config/master.key]
 set :linked_dirs,  %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system storage]
